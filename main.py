f4665fa1357fb910618841705ee458f5a4e11ba8
alessandroquirile <a.quirile@studenti.unina.it>
2023-06-27 15:45:28 +0200

    Updated gitignore file

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def extract(folder_path: str):
         with zipfile.ZipFile(folder_path, 'r') as zip_ref:
             zip_ref.extractall(extract_directory)
         print(folder_path, "extracted")
+        os.remove(folder_path)
+        print(folder_path, "deleted")
     else:
         print(folder_path, "does not exist")
 
