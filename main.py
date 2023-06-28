192c6873c7f061c0b7b09f4820c891744ccfbd9a
alessandroquirile <a.quirile@studenti.unina.it>
2023-06-28 11:27:39 +0200

    Implemented function which transforms dict values to list of strings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 
+
 def extract(folder_path: str):
     if os.path.exists(folder_path):
         extract_directory = "./"
@@ -10,4 +11,20 @@ def extract(folder_path: str):
     else:
         print(folder_path, "does not exist")
 
-extract("ted_talks_it.zip")
+
+def dict_values_to_list_of_strings(dictionary):
+    result = []
+    for value in dictionary.values():
+        if isinstance(value, list):
+            result.extend(str(element) for element in value)
+        else:
+            result.append(str(value))
+    return result
+
+
+
+# extract("ted_talks_it.zip")
+
+my_dict = {"name": "John", "age": 30, "hobbies": ["reading", "coding", "sports"]}
+result_list = dict_values_to_list_of_strings(my_dict)
+print(result_list)
