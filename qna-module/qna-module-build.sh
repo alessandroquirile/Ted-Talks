5c7d0f2f367122a4c5501561bab140c02b18230d
imatrisciano <ivanomatrisciano@gmail.com>
2023-06-30 12:04:32 +0200

    Added Q&A module

---
+++ qna-module/qna-module-build.sh
@@ -0,0 +1,2 @@
+git clone https://huggingface.co/timpal0l/mdeberta-v3-base-squad2
+docker build -f qna-module.Dockerfile -t qna-module .
---
+++ qna-module/qna-module.Dockerfile
@@ -0,0 +1,2 @@
+FROM semitechnologies/qna-transformers:custom
+COPY ./mdeberta-v3-base-squad2 /app/models/model
